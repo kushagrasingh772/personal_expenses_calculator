621820a787b71ccb67965340c84babcdcd037435
Kushagra Singh <52888710+perkymaster@users.noreply.github.com>
2020-09-18 15:54:13 +0530

    Update transactions.dart

--- lib/transactions.dart
+++ lib/transactions.dart
@@ -1,17 +1,15 @@
-import'package:flutter/material.dart';
-class Transactions{
+import 'package:flutter/foundation.dart';
 
+class Transaction {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
 
-
-
-  Transactions({
+  Transaction({
     @required this.id,
     @required this.title,
     @required this.amount,
-    @required this.date}
-    );
+    @required this.date,
+  });
 }
